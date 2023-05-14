407ff7ba9aa4b07a7bc161e229dcfa8d5968ec1e
Raphael <124627112+raphaelbnr@users.noreply.github.com>
2023-05-14 20:11:13 -0300

    Add files via upload

---
+++ src/js/pjpokem.js
@@ -0,0 +1,39 @@
+function mudarTema(){
+
+ let alterarTema =  document.querySelector('#botao-alterar-tema');
+ let imagemTrocaModo= document.querySelector('.imagem-botao')
+ let modo = document.querySelector('body')
+
+ modo.classList.toggle('modo-escuro')
+    
+ if(modo.classList.contains('modo-escuro')){
+    imagemTrocaModo.setAttribute('src', "./src/imagens/moon.png")
+}else{
+    imagemTrocaModo.setAttribute('src', "./src/imagens/sun.png")
+}
+
+}
+
+
+
+
+
+/*
+let alterarTema =  document.querySelector('#botao-alterar-tema');
+let imagemTrocaModo= document.querySelector('.imagem-botao')
+let modo = document.querySelector('body')
+
+alterarTema.addEventListener('click', ()=>{
+    modo.classList.toggle('modo-escuro')
+    
+     if(modo.classList.contains('modo-escuro')){
+        imagemTrocaModo.setAttribute('src', "./src/imagens/moon.png")
+    }else{
+        imagemTrocaModo.setAttribute('src', "./src/imagens/sun.png")
+    }
+  }
+)
+*/
+
+
+
